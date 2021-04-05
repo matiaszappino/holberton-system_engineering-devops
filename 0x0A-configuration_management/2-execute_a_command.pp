533c6fd190c7fb45dc62be21d44f081481d24776
Matias Zappino <matiaszappino31@gmail.com>
2021-04-05 23:57:43 +0000

    Fixed command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # Manifest that kills a process named killmenow
 
 exec { "killmenow":
-    path => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-    provider => 'shell'
     command => "/usr/bin/pkill killmenow",
 }
