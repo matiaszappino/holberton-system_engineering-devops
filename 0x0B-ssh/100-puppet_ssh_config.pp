76b203d4511689916b1f5908b864b684816c0f46
Matias Zappino <matiaszappino31@gmail.com>
2021-04-05 21:46:04 +0000

    Fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Changes to our configuration file
 
-changepass {"pass_change":
-    path  => "/etc/ssh/ssh_config",
-    line  => "    PasswordAuthentication no",
-    match =>  "PasswordAuthentication yes",
+file_line {'No_password':
+    path  => '/etc/ssh/ssh_config',
+    line  => '    PasswordAuthentication no',
+    match =>  'PasswordAuthentication yes',
 }
-changepass {"Identity_file":
-    path => "/etc/ssh/ssh_config",
-    line => "    IdentityFile ~/.ssh/holberton",
+file_line {'Key_file':
+    path => '/etc/ssh/ssh_config',
+    line => '    IdentityFile ~/.ssh/holberton',
 }
